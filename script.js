deb37fcd319d8af54052e997446bd6ed0a411338
minseob shim <shim5505@gmail.com>
2023-04-17 20:29:09 +0900

    포트폴리오 기본 설정

---
+++ script.js
@@ -0,0 +1,25 @@
+/** @type {HTMLCanvasElement} */
+const canvas = document.getElementById("canvas1");
+const ctx = canvas.getContext("2d");
+canvas.width = 800;
+canvas.height = 720;
+
+class InputHandler {
+  // 사용자 입력에 따라 캐릭터 이동 및 상호작용
+}
+
+class Player {
+  // 사용자 클래스
+}
+
+class Background {
+  // 배경 클래스
+}
+
+function displayStatusText() {
+  // 사용자가 볼 수 있는 텍스트를 추가
+}
+
+function animate() {
+  // 기본 구동
+}
