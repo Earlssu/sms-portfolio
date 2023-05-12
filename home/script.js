652ac23186e78b5ecc0f83aee63f0953c5853b77
minseob shim <shim5505@gmail.com>
2023-05-12 22:32:31 +0900

    [JS]유효성 검사 추가

--- home/script.js
+++ home/script.js
@@ -1,24 +1,58 @@
-document
-  .getElementById("contact_form")
-  .addEventListener("submit", function (event) {
-    event.preventDefault();
-    if (window.confirm("이메일을 보내시겠습니까?")) {
-      emailjs
-        .sendForm("service_sms_portfolio", "template_sms_portfolio", this)
-        .then(
-          function () {
-            alert("입력하신 내용을 보냈습니다.");
-            document.getElementById("from_name").value = "";
-            document.getElementById("from_phone").value = "";
-            document.getElementById("from_email").value = "";
-            document.getElementById("from_message").value = "";
-          },
-          function (error) {
-            console.log("FAILED...", error);
-          }
-        );
-    }
-  });
+const phoneNumber = document.getElementById("from_phone");
+const phoneValidationMessage = document.getElementById(
+  "phone_validation_message"
+);
+
+phoneNumber.addEventListener("keyup", () => {
+  phoneNumber.value = phoneNumber.value
+    .replace(/[^0-9]/g, "")
+    .replace(/^(\d{0,3})(\d{0,4})(\d{0,4})$/g, "$1-$2-$3")
+    .replace(/(\-{1,2})$/g, "");
+});
+
+phoneNumber.addEventListener("change", () => {
+  console.log(phoneNumber.value.length);
+  if (phoneNumber.value.length < 13) {
+    phoneValidationMessage.textContent =
+      "휴대폰 번호를 모두 입력하지 않았습니다.";
+  }
+});
+
+const emailInput = document.getElementById("from_email");
+const emailValidationMessage = document.getElementById(
+  "email_validation_message"
+);
+const emailValidation =
+  /^[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*@[0-9a-zA-Z]([-_.]?[0-9a-zA-Z])*.[a-zA-Z]{2,3}$/i;
+
+emailInput.addEventListener("change", () => {
+  console.log(emailInput.value.match(emailValidation));
+  if (emailInput.value.match(emailValidation) === null) {
+    emailValidationMessage.textContent =
+      "이메일을 맞게 입력했는지 확인해주세요.";
+  }
+});
+
+document.getElementById("contact_form").addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (window.confirm("입력하신 내용으로 보내시겠습니까?")) {
+    emailjs
+      .sendForm("service_sms_portfolio", "template_sms_portfolio", this)
+      .then(
+        () => {
+          alert("감사합니다! 빠른 시간 안에 답장드리도록 하겠습니다.");
+          document.getElementById("from_name").value = "";
+          document.getElementById("from_phone").value = "";
+          document.getElementById("from_email").value = "";
+          document.getElementById("from_message").value = "";
+        },
+        function (error) {
+          console.log("이메일을 보내지 못했습니다.", error);
+        }
+      );
+  }
+});
 
 const p1Slides = document.querySelectorAll(".project_01_slide");
 const p1NextBtn = document.querySelector(".btn_01_next");
@@ -32,7 +66,7 @@ p1Slides.forEach((slide, idx) => {
   p1SliderPageNum.textContent = `${p1currentIdx + 1} / ${p1MaxIdx + 1}`;
 });
 
-p1NextBtn.addEventListener("click", function () {
+p1NextBtn.addEventListener("click", () => {
   if (p1currentIdx === p1MaxIdx) {
     p1currentIdx = 0;
   } else {
@@ -45,7 +79,7 @@ p1NextBtn.addEventListener("click", function () {
   });
 });
 
-p1PrevBtn.addEventListener("click", function () {
+p1PrevBtn.addEventListener("click", () => {
   if (p1currentIdx === 0) {
     p1currentIdx = p1MaxIdx;
   } else {
@@ -57,7 +91,6 @@ p1PrevBtn.addEventListener("click", function () {
   });
 });
 
-//
 const p2Slides = document.querySelectorAll(".project_02_slide");
 const p2NextBtn = document.querySelector(".btn_02_next");
 const p2PrevBtn = document.querySelector(".btn_02_prev");
@@ -70,7 +103,7 @@ p2Slides.forEach((slide, idx) => {
   p2SliderPageNum.textContent = `${p2currentIdx + 1} / ${p2MaxIdx + 1}`;
 });
 
-p2NextBtn.addEventListener("click", function () {
+p2NextBtn.addEventListener("click", () => {
   if (p2currentIdx === p2MaxIdx) {
     p2currentIdx = 0;
   } else {
@@ -83,7 +116,7 @@ p2NextBtn.addEventListener("click", function () {
   });
 });
 
-p2PrevBtn.addEventListener("click", function () {
+p2PrevBtn.addEventListener("click", () => {
   if (p2currentIdx === 0) {
     p2currentIdx = p2MaxIdx;
   } else {
